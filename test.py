93a5921c9637f89c3b7824ede563b978b63651e4
Poopie <elstuhnofficial@gmail.com>
2021-12-06 09:13:24 +0800

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,13 @@ from tensorflow.keras.datasets import mnist
 from time import sleep
 from matplotlib import image
 import matplotlib.pyplot as plt
-pic = image.imread('test.png')
 model = load_model('CNNmodel.hdf5')
 (trainX, trainy), (testX, testy) = mnist.load_data()
 trainX, testX = trainX / 255.0, testX / 255.0
-pic = pic / 255.0
-pic.resize(28, 28, 1)
 plt.figure(figsize=(10,10))
 plt.title("Picture Input")
 plt.imshow(testX[143])
 plt.show()
-#prediction = model.predict(pic.reshape(1, 28, 28, 1))
 prediction = model.predict(testX[143].reshape(1, 28, 28, 1))
 print(f"Prediction: {prediction.argmax()}")
 print(f"Actual: {testy[143]}")
@@ -37,4 +33,4 @@ def check_acc(model, X_test, y_test):
     plt.show()
     print(f"Accuracy: {(correct/(correct+incorrect))*100}%")
 
-check_acc(model, testX, testy
+check_acc(model, testX, testy)
